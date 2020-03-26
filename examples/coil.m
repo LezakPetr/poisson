ab134fd12ea5ccae7bace4bd8d04805ee72cd863
LezakPetr <>
2020-03-26 21:56:08 +0100

    Coil integral

--- examples/coil.m
+++ examples/coil.m
@@ -1,38 +1,69 @@
 
 R1 = 10e-3;
 R2 = 15e-3;
-L = 40e-3;
+S = 20e-3;
 N = 1000;
 mu = 4e-7 * pi;
 
 steps = 10;
 seq = 0:(steps - 1);
 
-sum1 = 0;
+# Dlouha civka
+Rs = (R1 + R2) / 2;
+L1 = mu * pi * Rs^2 * N^2 / S;
+L1
 
-for iRa = steps
+# Uplny vypocet integralu
+diff2 = (S / steps)^2 * ((R2 - R1) / steps)^2 * (2 * pi / steps);
+sum2 = 0;
+
+for iRa = seq
 	Ra = R1 + (0.5 + iRa) / steps * (R2 - R1);
 	
-	for iRb = steps
+	for iRb = seq
 		Rb = R1 + (0.5 + iRb) / steps * (R2 - R1);
 		
-		for iLa = steps
-			la = (0.5 + iLa) / steps * L;
+		for iLa = seq
+			la = (0.5 + iLa) / steps * S;
 			
-			for iLb = steps
-				lb = (0.5 + iLb) / steps * L;
+			for iLb = seq
+				lb = (0.5 + iLb) / steps * S;
 				
-				for iAlpha = steps
-					alpha = (0.5 + iAlpha) / steps * L;
-					
-					sum1 += Ra * Rb / sqrt((lb - la)^2 + (Rb - Ra * cos(alpha))^2 + (Ra * sin(alpha))^2);
+				for iAlpha = seq
+					alpha = (0.5 + iAlpha) / steps * 2 * pi;
+					r = sqrt((lb - la)^2 + (Rb - Ra * cos(alpha))^2 + (Ra * sin(alpha))^2);
+					sum2 += Ra * Rb / r;
 				endfor
 			endfor
 		endfor
 	endfor
 endfor
 
-L1 = (N * mu) / (2 * (R2 - R1)) * sum1;
+L2 = (N^2 * mu * diff2) / (2 * (R2 - R1)^2 * S^2) * sum2;
+L2
 
-L1
+# Uplny vypocet integralu
+diff3 = (S / steps) * ((R2 - R1) / steps)^2 * (2 * pi / steps);
+sum3 = 0;
+
+for iRa = seq
+	Ra = R1 + (0.5 + iRa) / steps * (R2 - R1);
+	
+	for iRb = seq
+		Rb = R1 + (0.5 + iRb) / steps * (R2 - R1);
+		
+		for it = seq
+			t = (0.5 + it) / steps * S;
+							
+			for iAlpha = seq
+				alpha = (0.5 + iAlpha) / steps * 2 * pi;
+				r = sqrt(t^2 + (Rb - Ra * cos(alpha))^2 + (Ra * sin(alpha))^2);
+				sum3 += Ra * Rb * (S - t) / r;
+			endfor
+		endfor
+	endfor
+endfor
+
+L3 = (N^2 * mu * diff3) / ((R2 - R1)^2 * S^2) * sum3;
+L3
 
