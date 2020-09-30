8f4c390b4f700f63d6448d82c323d08ed0383d86
Petr Lezak <PetrLezakCZ@gmail.com>
2020-09-30 07:06:24 +0200

    Oprava deriavaci souradneho systemu

--- test/tenzory.m
+++ test/tenzory.m
@@ -1,29 +1,59 @@
 
 pkg load statistics;
 
+rehash();
+
 cylindrical.p = @(x, y, z) [ sqrt(x^2 + y^2), atan2(y, x), z ]; 
 cylindrical.pInv = @(r, alpha, z) [ r * cos(alpha), r * sin(alpha), z ];
+
+cylindrical.dp = @(x, y, z) [
+	x / sqrt(x^2 + y^2), y / sqrt(x^2 + y^2), 0;
+	-y / (x^2 + y^2), x / (x^2 + y^2), 0;
+	0, 0, 1
+];
+
+cylindrical.dpInv = @(r, alpha, z) [
+	cos(alpha), -r * sin(alpha), 0;
+	sin(alpha), r * cos(alpha), 0;
+	0, 0, 1
+];
 
 spherical.p = @(x, y, z) [ sqrt(x^2 + y^2 + z^2), atan2(y, x), atan(z / (sqrt(x^2 + y^2))) ]; 
 spherical.pInv = @(r, alpha, beta) [ r * cos(alpha) * cos(beta), r * sin(alpha) * cos(beta), r * sin(beta) ];
 
-systems = { cylindrical, spherical };
-eps = 1e-9;
+spherical.dp = @(x, y, z) [
+	x / sqrt(x^2 + y^2 + z^2), y / sqrt(x^2 + y^2 + z^2), z / sqrt(x^2 + y^2 + z^2);
+	-y / (x^2 + y^2), x / (x^2 + y^2), 0;
+	-x*z / ((x^2 + y^2 + z^2) * sqrt(x^2 + y^2)), -y*z / ((x^2 + y^2 + z^2) * sqrt(x^2 + y^2)), sqrt(x^2 + y^2) / (x^2 + y^2 + z^2)
+];
 
-# Inverze
-for s = systems
-	sys = s{};
+spherical.dpInv = @(r, alpha, beta) [
+	cos(alpha) * cos(beta), -r * sin(alpha) * cos(beta), -r * cos(alpha) * sin(beta);
+	sin(alpha) * cos(beta), r * cos(alpha) * cos(beta), -r * sin(alpha) * sin(beta);
+	sin(beta), 0, r * cos(beta)
+];
 
-	for i = 1:100
-		x = normrnd(0, 1);
-		y = normrnd(0, 1);
-		z = normrnd(0, 1);
+systems = {cylindrical,	spherical };
+
+eps = 1e-10;
+
+for i = 1:1
+	x = normrnd(0, 1);
+	y = normrnd(0, 1);
+	z = normrnd(0, 1);
 		
-		curved = sys.p(x, y, z);
-		cartesian = sys.pInv(curved(1), curved(2), curved(3));
+	for s = systems
+		sys = s{};
+
+		# Inverze souradneho systemu
+		curvedCoord = sys.p(x, y, z);
+		cartesianCoord = sys.pInv(curvedCoord(1), curvedCoord(2), curvedCoord(3));
 		
-		assert(cartesian(1), x, eps);
-		assert(cartesian(2), y, eps);
-		assert(cartesian(3), z, eps);
+		assert([x, y, z], cartesianCoord, eps);
+
+		# Provazanost prvnich derivaci souradneho systemu
+		firstDerivatesProduct = sys.dp(x, y, z) * sys.dpInv(curvedCoord(1), curvedCoord(2), curvedCoord(3));
+
+		assert(eye(3), firstDerivatesProduct, eps);
 	endfor
 endfor
