9ba78a0e922c3dae58d133c39faab1504dc30985
Petr Lezak <PetrLezakCZ@gmail.com>
2020-10-13 21:20:00 +0200

    Removed calculator

--- calculator/src/poisson/expression/Expression.java
+++
@@ -1,9 +0,0 @@
-package poisson.expression;
-
-import java.util.Map;
-
-public interface Expression {
-    public Value evaluate (final Map<Variable, Value> variableValueMap);
-    public boolean dependsOn (final Variable variable);
-    public Expression differentiate (final Variable variable);
-}
