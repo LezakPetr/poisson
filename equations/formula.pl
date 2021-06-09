8c40efb81bd2a2e5c7b7a2ff5b341eca9cf50d48
Petr Lezak <PetrLezakCZ@gmail.com>
2021-06-09 21:43:49 +0200

    Seriovy rezonancni obvod

--- equations/formula.pl
+++ equations/formula.pl
@@ -20,12 +20,15 @@ verify_true(_) :-
 	writeln("Formula not truthful"),
 	fail.
 
+rewrite_formula_and_validate(Formula) :-
+	rewrite_expression(Formula, RewrittenFormula),
+	log(["Rewritten formula: ", RewrittenFormula]),
+	verify_formula(RewrittenFormula).
+	
 % Validates formula (checks that it is true) and prints it.
 print_validated_formula(Label, Formula) :-
 	copy_term(Formula, CopiedFormula),
-	rewrite_expression(Formula, RewrittenFormula),
-	log(["Rewritten formula: ", RewrittenFormula]),
-	verify_formula(RewrittenFormula),
+	rewrite_formula_and_validate(Formula),
 	print_formula(Label, CopiedFormula).
 
 
@@ -36,4 +39,12 @@ print_formula(Label, CopiedFormula) :-
 print_formula(_, _) :-
 	writeln("Formula cannot be printed"),
 	fail.
+
+
+print_validated_multiformula(_, []).
+
+print_validated_multiformula(Declarations, [formula(Label, Line) | LineTail]) :-
+	copy_term(declare(Declarations, Line), Formula),
+	print_validated_formula(Label, Formula),
+	print_validated_multiformula(Declarations, LineTail).
 
